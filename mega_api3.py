771f695b349e2b1b959b55f7f0ae252c284a58c3
Welton Vaz <weltonvaz@gmail.com>
2023-02-17 07:30:33 -0300

    alterações do dia 16/02/2023

--- mega_api3.py
+++ mega_api3.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from loteria_caixa import MegaSena
+from datetime import date, timedelta
+from datetime import datetime
 
 # Abrindo um arquivo para escrita
 with open("resultados_megasena.txt", "w") as file:
@@ -7,14 +9,15 @@ with open("resultados_megasena.txt", "w") as file:
     file.write("numero_concurso,dezenas_sorteadas\n")
 
     # Loop para baixar os resultados de todos os sorteios
-    for i in range(2562, 2565):
+    for i in range(1, 2565):
         concurso = MegaSena(i)
         
         dados = concurso.todosDados()
+        data = datetime.strptime(concurso.dataApuracao(), "%d/%m/%Y").date()
         
         numero = dados.get('numero')
         if numero:
-            file.write("{},{}\n".format(numero, " ".join(concurso.listaDezenas())))
+            file.write("{},{},{}\n".format(data.toordinal(),numero, " ".join(concurso.listaDezenas())))
         else:
             print("Não foi possível obter o número do concurso")
         
